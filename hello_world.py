fb7739bbe3c5e3f6bce51521759daa1aaf30a3f6
Mateusz Błaszczyk <mateusz.blaszczyk@fitqbe.com>
2019-04-18 12:53:08 +0200

    Implemetcja liczb pierwszych

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,18 @@
-print("Hello world")
-print("Hello world again")
+print("Sprawdz, czy podana liczba jest liczba pierwsza!")
+
+x = input("Podaj liczbe: ")
+
+
+
+def liczby(x, z=1):
+    while int(x) > z:
+        if int(x) / z != 0:
+            z += 1
+            if int(x) == z:
+                return "To jest liczba pierwsza!"
+        else:
+            return "To nie jest liczba pierwsza!"
+
+
+print(liczby(x, z=1))
+
