1d474c069244a578ce16fad0b4f0d42814b3d069
Mateusz Błaszczyk <mateusz.blaszczyk@fitqbe.com>
2019-04-18 13:07:00 +0200

    2 wersja mojego programu

--- hello_world.py
+++ hello_world.py
@@ -1,18 +1,18 @@
-print("Sprawdz, czy podana liczba jest liczba pierwsza!")
+print("Is it a Prime Number? Check out!")
 
-x = input("Podaj liczbe: ")
+number_to_check = input("Your Number: ")
 
 
 
-def liczby(x, z=1):
-    while int(x) > z:
-        if int(x) / z != 0:
-            z += 1
-            if int(x) == z:
-                return "To jest liczba pierwsza!"
+def number(number_to_check, factor=1):
+    while int(number_to_check) > factor:
+        if int(number_to_check) / factor != 0:
+            factor += 1
+            if int(number_to_check) == factor:
+                return "It is Prime Number!"
         else:
-            return "To nie jest liczba pierwsza!"
+            return "It isn't Prime Number!"
 
 
-print(liczby(x, z=1))
+print(number(number_to_check, factor=1))
 
